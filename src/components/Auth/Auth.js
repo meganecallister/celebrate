2c7e0e98e888ed243aeeb84710cc66aec99252e9
Megan Callister <meganecallister@gmail.com>
2018-04-19 10:56:38 -0600

    added env.prod

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -5,16 +5,23 @@ import cupcake from '../../cupcake-logo.png';
 export default function Auth() {
     return (
         <div className='auth'>
-            <div className='left-side'>
+        <menu>
+            {/* <img src={cupcake} alt='delicious chocolate cupcake'/>
+            <p>Celebrate</p> */}
+            <a href={ process.env.REACT_APP_LOGIN }>
+                <button className='link-button'>Log in</button>
+            </a>
+        </menu>
+            {/* <div className='left-side'> */}
                 <h1>Celebrate</h1>
                 <img src={cupcake} alt='delicious chocolate cupcake'/>
-            </div>
-            <div className='right-side'>
+            {/* </div>
+            <div className='right-side'> */}
                 <p className='catch-phrase'>Join us in celebrating the lives of others.</p>
                 <a href={ process.env.REACT_APP_LOGIN }>
                     <button className='auth-button'>click here!</button>
                 </a>
-            </div>
+            {/* </div> */}
         </div>
     )    
 }
